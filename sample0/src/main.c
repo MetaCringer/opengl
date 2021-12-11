21ad354c4180580360f521175bbd1e7623dc5cad
metacringer <metacringer@gmail.com>
2021-12-11 23:42:40 +0200

    main updated

--- sample0/src/main.c
+++ sample0/src/main.c
@@ -6,11 +6,6 @@
 int width=640, height=480;
 double time;
 
-float vertices[] = {
-    0.0f,  0.5f, // Vertex 1 (X, Y)
-    0.5f, -0.5f, // Vertex 2 (X, Y)
-    -0.5f, -0.5f  // Vertex 3 (X, Y)
-};
 
 void init(void)
 {
