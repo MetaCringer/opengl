5b9fb3c0e332d2b6ee269dff760a25546a2b7dce
metacringer <metacringer@gmail.com>
2021-12-12 17:42:32 +0200

    GLFW remade

--- sample0/src/main.c
+++ sample0/src/main.c
@@ -18,19 +18,32 @@ void init(void)
     glOrtho(0.0,1.0,0.0,1.0,-1.0,1.0);
 }
 
+double dx(int x){
+    return (x + (width/2)) / width;
+}
+
+double dy(int y){
+    return (y + (height/2)) / height;
+}
+
 void display(){
       // clear all pixels
     glClear(GL_COLOR_BUFFER_BIT);
 
     //  draw white polygon with corners at (0.25,0.25,0.0) and (0.75,0.75,0.0)    
     glColor3f(1.0,1.0,1.0);
-    glBegin(GL_POLYGON);
+    glBegin(GL_LINE_STRIP);
         glVertex3f(0.25,0.25,0.0);
         glVertex3f(0.75,0.25,0.0);
         glVertex3f(0.75,0.75,0.0);
         glVertex3f(0.25,0.75,0.0);
-    glEnd();   
-
+    glEnd();
+    glBegin(GL_POINT);
+        glVertex3f(0.25,0.25,0.0);
+        glVertex3f(0.75,0.25,0.0);
+        glVertex3f(0.75,0.75,0.0);
+        glVertex3f(0.25,0.75,0.0);
+    glEnd();
     // don't wait, start processing buffered OpenGL routines
     glFlush();
 }
