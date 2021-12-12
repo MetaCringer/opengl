3c56700eb0e830e65ce0339b7f8740f5377083ec
metacringer <metacringer@gmail.com>
2021-12-12 19:32:55 +0200

    display() move above

--- sample0/src/main.c
+++ sample0/src/main.c
@@ -7,6 +7,29 @@ int width=640, height=480;
 double time;
 GLFWwindow* window;
 
+void display(){
+      // clear all pixels
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //  draw white polygon with corners at (0.25,0.25,0.0) and (0.75,0.75,0.0)    
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_LINE_STRIP);
+        glVertex3f(0.25,0.25,0.0);
+        glVertex3f(0.75,0.25,0.0);
+        glVertex3f(0.75,0.75,0.0);
+        glVertex3f(0.25,0.75,0.0);
+    glEnd();
+    glBegin(GL_POINT);
+        glVertex3f(0.25,0.25,0.0);
+        glVertex3f(0.75,0.25,0.0);
+        glVertex3f(0.75,0.75,0.0);
+        glVertex3f(0.25,0.75,0.0);
+    glEnd();
+    // don't wait, start processing buffered OpenGL routines
+    glFlush();
+}
+
+
 void init(void)
 {
     // select clearing (background) color 
@@ -30,27 +53,7 @@ double dy(int y){
     return (y + (height/2)) / height;
 }
 
-void display(){
-      // clear all pixels
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    //  draw white polygon with corners at (0.25,0.25,0.0) and (0.75,0.75,0.0)    
-    glColor3f(1.0,1.0,1.0);
-    glBegin(GL_LINE_STRIP);
-        glVertex3f(0.25,0.25,0.0);
-        glVertex3f(0.75,0.25,0.0);
-        glVertex3f(0.75,0.75,0.0);
-        glVertex3f(0.25,0.75,0.0);
-    glEnd();
-    glBegin(GL_POINT);
-        glVertex3f(0.25,0.25,0.0);
-        glVertex3f(0.75,0.25,0.0);
-        glVertex3f(0.75,0.75,0.0);
-        glVertex3f(0.25,0.75,0.0);
-    glEnd();
-    // don't wait, start processing buffered OpenGL routines
-    glFlush();
-}
 
 int main(int argc, char** argv){
     printf("GLFW init ...'\n");
